50fc3392ba50b6f5eb1ca43b14fec0a24fc00c61
Thomas Clark <tnc1997@outlook.com>
2024-10-18 20:08:09 +0100

    feat: add custom post auth page

--- lib/google_sign_in_desktop.dart
+++ lib/google_sign_in_desktop.dart
@@ -80,6 +80,7 @@ class GoogleSignInDesktop extends GoogleSignInPlatform {
 
   late String _clientId;
   late String _clientSecret;
+  String? _customPostAuthPage;
   late List<String> _scopes;
   late GoogleSignInDesktopStore<GoogleSignInDesktopTokenData> _tokenDataStore;
   GoogleSignInUserData? _userData;
@@ -131,6 +132,13 @@ class GoogleSignInDesktop extends GoogleSignInPlatform {
     String clientSecret,
   ) {
     _clientSecret = clientSecret;
+  }
+
+  /// Sets the HTML for the page that is shown to the user after they have authenticated successfully.
+  set customPostAuthPage(
+    String? customPostAuthPage,
+  ) {
+    _customPostAuthPage = customPostAuthPage;
   }
 
   /// Sets the token data store that is used to store tokens between sessions.
@@ -432,7 +440,8 @@ class GoogleSignInDesktop extends GoogleSignInPlatform {
         request.response
           ..statusCode = 200
           ..headers.set('content-type', 'text/html; charset=UTF-8')
-          ..write('''<!DOCTYPE html>
+          ..write(_customPostAuthPage ??
+              '''<!DOCTYPE html>
         <html>
           <head>
             <meta charset="utf-8">
